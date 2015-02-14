49592ee7806b6353986968bd92a7fb06a9f2962a
jun nakano <jurin.nakano@gmail.com>
2015-02-14 12:39:42 +0900

    jQueryのeach関数を試した。 http://oss.infoscience.co.jp/jquery/Core/each.html#callback

--- tutorials/custum.js
+++ tutorials/custum.js
@@ -1,15 +1,25 @@
 $(function(){
-    $("a").click(
-      function(){
-        alert("test $()");
-    });
-});
-$(function(){
   $("#orderedlist").addClass("red");
   $("#orderedlist > li").addClass("blue");
   $("#orderedlist > li").hover(function(){
      $(this).addClass("green");
   },function(){
      $(this).removeClass("green");
+  });
+  $("#orderedlist").find("li").each(function(i){
+     // ↓eachのAPI仕様
+     // http://oss.infoscience.co.jp/jquery/Core/each.html
+     //if(i ==1) return true; //処理中断して次のループ処理。
+     //if(i ==1) return false;//ループ終了。以降は処理なし。
+     $(this).append(" YAN!" + i);
+  });
+});
+
+$(function(){
+  $("#reset").click(function(){
+//    $("#form")[0].reset();
+    $("form").each(function(i){
+      this.reset(); //←動かないorz。 don't work.
+    });
   });
 });
