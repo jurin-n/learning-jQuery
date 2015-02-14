54cde148ae6deb50b5a3b1e48f8dba1873efeaab
jun nakano <jurin.nakano@gmail.com>
2015-02-14 13:14:12 +0900

    アンカーを選択するサンプルためした。

--- tutorials/custum.js
+++ tutorials/custum.js
@@ -27,5 +27,8 @@ $(function(){
   $("li").not(":has(ul)").css("border","1px solid black");
   //http://api.jquery.com/filter/
   //$("li").filter(":has(ul)").css("border","1px solid black");
-
+  $("a[href='/content/gallery']").click(function(){
+    alert("/content/gallery /^o^/");
+    return false;
+  });
 });
